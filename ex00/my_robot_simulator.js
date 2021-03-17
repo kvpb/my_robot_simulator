9f26258c5e47694514485e560930f0cc8ec82e24
karl bertin <bertin_k@qwasar.io>
2021-03-17 14:16:43 +0000

    my_robot_simulator updated: ex00/ created, ex00/my_robot_simulator.js created, and my_robot_simulator.js deleted.

--- ex00/my_robot_simulator.js
+++ ex00/my_robot_simulator.js
@@ -1,43 +1,57 @@
+/**
+ * Copyright (c) 2021
+ *
+ * https://upskill.us.qwasar.io/projects/my_robot_simulator
+ *
+ * @summary A robot simulator.
+ * @author onoja_a <local-part@domain>
+ * @author bertin_k <local-part@domain>
+ *
+ * Created 
+ * Last modified 
+ */
+
 const directions = ["north", "east", "south", "west"];
-function my_robot_simulator(instruction) {
-  var movement = Array.from(instruction);
-  var bearing = "north";
+
+function my_robot_simulator(instructions) {
+  var movements = Array.from(instructions);
+  var bearing = 'north';
   var get_bearing;
   var x = 0;
   var y = 0;
-  for (var i = 0; i < movement.length; i++) {
-    if (movement[i] === "R") {
-      get_bearing = (directions.indexOf(bearing) + 1) % 4;
+  var i = 0;
+  var n = movements.length;
+
+  for ( i = 0; i < n; i++ ) {
+    if (movements[i] == 'L') {
+      get_bearing = ( directions.indexOf(bearing) + 3 ) % 4;
       bearing = directions[get_bearing];
-    } else if (movement[i] === "L") {
-      get_bearing = (directions.indexOf(bearing) + 3) % 4;
+    } else if (movements[i] == 'R') {
+      get_bearing = ( directions.indexOf(bearing) + 1 ) % 4;
       bearing = directions[get_bearing];
-    } else if (movement[i] === "A") {
+    } else if (movements[i] == 'A') {
       switch (bearing) {
-        case "north":
+        case 'north':
           y++;
           break;
-        case "east":
-          x++;
+        case 'west':
+          x--;
           break;
-        case "south":
+        case 'south':
           y--;
           break;
-        case "west":
-          x--;
+        case 'east':
+          x++;
           break;
         default:
           return x;
       }
     } else {
-        return 'Instruction given is invalid';
+        return 'Error: Invalid instruction.';
     }
   }
-  if(y != 0) {
+  if ( y != 0 ) {
       y *= -1;
   }
   return `{x: ${x}, y: ${y}, bearing: '${bearing}'}`;
-  
-
 }
-
