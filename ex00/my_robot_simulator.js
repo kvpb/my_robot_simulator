00c05a6ddbf0d92a3d2f52a88bac45e6577287e0
Karl <local-part@domain>
2022-12-15 21:52:37 +0100

    [my_robot_simulator/ex00/] my_robot_simulator.js updated: released My Robot Simulator.

--- ex00/my_robot_simulator.js
+++ ex00/my_robot_simulator.js
@@ -13,45 +13,57 @@
 
 const directions = ["north", "east", "south", "west"];
 
-function my_robot_simulator(instructions) {
-  var movements = Array.from(instructions);
-  var bearing = 'north';
-  var get_bearing;
-  var x = 0;
-  var y = 0;
-  var i = 0;
-  var n = movements.length;
+function my_robot_simulator(instructions)
+{
+	var movements = Array.from(instructions);
+	var bearing = 'north';
+	var get_bearing;
+	var x = 0;
+	var y = 0;
+	var i = 0;
+	var n = movements.length;
 
-  for ( i = 0; i < n; i++ ) {
-    if (movements[i] == 'L') {
-      get_bearing = ( directions.indexOf(bearing) + 3 ) % 4;
-      bearing = directions[get_bearing];
-    } else if (movements[i] == 'R') {
-      get_bearing = ( directions.indexOf(bearing) + 1 ) % 4;
-      bearing = directions[get_bearing];
-    } else if (movements[i] == 'A') {
-      switch (bearing) {
-        case 'north':
-          y++;
-          break;
-        case 'west':
-          x--;
-          break;
-        case 'south':
-          y--;
-          break;
-        case 'east':
-          x++;
-          break;
-        default:
-          return x;
-      }
-    } else {
-        return 'Error: Invalid instruction.';
-    }
-  }
-  if ( y != 0 ) {
-      y *= -1;
-  }
-  return `{x: ${x}, y: ${y}, bearing: '${bearing}'}`;
+	for ( i = 0; i < n; i++ )
+	{
+		if (movements[ i ] == 'L')
+		{
+			get_bearing = ( directions.indexOf(bearing) + 3 ) % 4;
+			bearing = directions[get_bearing];
+		}
+		else if (movements[ i ] == 'R')
+		{
+			get_bearing = ( directions.indexOf(bearing) + 1 ) % 4;
+			bearing = directions[get_bearing];
+		}
+		else if (movements[ i ] == 'A')
+		{
+			switch (bearing)
+			{
+				case 'north':
+					y++;
+					break;
+				case 'west':
+					x--;
+					break;
+				case 'south':
+					y--;
+					break;
+				case 'east':
+					x++;
+					break;
+				default:
+					return x;
+			}
+		}
+		else
+		{
+			return 'Error: Invalid instruction.';
+		}
+	}
+	if ( y != 0 )
+	{
+			y *= -1;
+	}
+
+	return `{x: ${x}, y: ${y}, bearing: '${bearing}'}`;
 }
